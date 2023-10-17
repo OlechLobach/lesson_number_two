da447d37f676b6e063ba93899df6664e311b2b1e
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-17 20:37:55 +0300

    lab/ex3/DONE

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
-#include <iostrem>
+#include <iostream>
+using namespace std;
 
 int main() {
-	return 0;
+    double v, t, a;
+    cout << "Enter initial velocity (v): ";
+    cin >> v;
+    cout << "Enter time duration (t): ";
+    cin >> t;
+    cout << "Enter acceleration (a): ";
+    cin >> a;
+
+    double S = v * t + 0.5 * a * t * t;
+
+  
+    cout << "Distance traveled: " << S << endl;
+
+    return 0;
 }
