e2456f3cb765bf46f1ddf2db0d47779504fc3082
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-17 20:14:43 +0300

    ex_first_DONE

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
-#include <iostrem>
+#include <iostream>
 
 int main() {
-	return 0;
+    double R1, R2, R3;
+    std::cout << "Enter the value of resistor R1: ";
+    std::cin >> R1;
+    std::cout << "Enter the value of resistor R2: ";
+    std::cin >> R2;
+    std::cout << "Enter the value of resistor R3: ";
+    std::cin >> R3;
+    double R0 = 1.0 / (1.0 / R1 + 1.0 / R2 + 1.0 / R3);
+    std::cout << "The equivalent resistance R0 is: " << R0 << " ohms" << std::endl;
+
+    return 0;
 }
