b98a9057258c975749ab1a0dedeb3acabb317383
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-17 20:33:43 +0300

    lab/ex2/done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-#include <iostrem>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
 
-int main() {
-	return 0;
+using namespace std;
+
+int main()
+{
+        double circumference;
+        std::cout << "Enter the circumference of the circle: ";
+        std::cin >> circumference;
+
+        double radius = circumference / (2.0 * M_PI);  
+        double area = M_PI * pow(radius, 2);  
+
+        std::cout << "Radius: " << radius << std::endl;
+        std::cout << "Area: " << area << std::endl;
+
+
+    return 0;
 }
